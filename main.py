b8f40e05473b20bc2ef3febcb6911f358f08d6e3
pcpeasant <p.g.tharwani@gmail.com>
2021-03-09 16:48:08 +0530

    Add comments

--- main.py
+++ main.py
@@ -2,9 +2,11 @@ import sys
 from PyQt5 import QtWidgets, uic
 import requests
 
+# Read the UI file that QtDesigner spit out
 qtcreator_file = "gui.ui"
 Ui_MainWindow, QtBaseClass = uic.loadUiType(qtcreator_file)
 
+# Pulls currency exchange table from random free API I found online
 response = requests.get('https://api.exchangeratesapi.io/latest').json()
 currencies = sorted(list(response['rates'].keys()))
 
@@ -17,17 +19,20 @@ class MyWindow(QtWidgets.QMainWindow, Ui_MainWindow):
         self.inputCurrency.addItems(currencies)
         self.outputCurrency.addItems(currencies)
 
+        # All the buttons and input boxes
         self.swapButton.clicked.connect(self.swap_values)
         self.inputBox.valueChanged.connect(self.convert_currency)
         self.inputCurrency.currentIndexChanged.connect(self.convert_currency)
         self.outputCurrency.currentIndexChanged.connect(self.convert_currency)
 
+    # Swap currencies when switch button is pressed
     def swap_values(self):
         inputcurr = int(self.inputCurrency.currentIndex())
         outputcurr = int(self.outputCurrency.currentIndex())
         self.outputCurrency.setCurrentIndex(inputcurr)
         self.inputCurrency.setCurrentIndex(outputcurr)
 
+    # Do the actual conversion
     def convert_currency(self):
         value = self.inputBox.value()
         inputcurr = str(self.inputCurrency.currentText())
